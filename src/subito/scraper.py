59ab2b61104007864e990a6503c441f094693c2c
Giovanni Piccolo <58179857+azzzoto@users.noreply.github.com>
2023-06-04 17:02:49 +0200

    Update scraper.py

--- src/subito/scraper.py
+++ src/subito/scraper.py
@@ -33,7 +33,7 @@ def df_maker_subito(url):
             #zuppa = bs4.BeautifulSoup(descrizione.text, "html.parser")
             ##text_descr = zuppa.find(class_ = "grid_detail-component__7sBtj grid_description__rEv3i")
             ##annuncio_txt_descr = text_descr.find('p').contents[0]
-#
+
             ##qui data
             #data_aggiunta = zuppa.find(class_ = "index-module_sbt-text-atom__ed5J9 index-module_token-caption__TaQWv size-normal index-module_weight-book__WdOfA index-module_insertion-date__MU4AZ")
             #data_annuncio = data_aggiunta.text
